664d075f6438c7fec2217333dd9a2f9b06b2a4e2
velykitty <jiyunee13@gmail.com>
2021-02-22 19:20:49 +0900

    onInsert -todos 객체에 사용자가 입력한 todo 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,10 +22,27 @@ const App = () => {
     },
   ]);
 
+  //고윳값으로 사용될 id
+  //ref를 사용해 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current, 
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo)); // concat으로 복제한 뒤 set
+      nextId.current += 1; // nextId 1씩 더하기
+    }, 
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList todos={todos}/>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   )
 }

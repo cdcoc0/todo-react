dd7222ef86177f06bce589a96b66554de9752c2f
velykitty <jiyunee13@gmail.com>
2021-02-22 21:36:52 +0900

    TodoInsert -onSubmit 이벤트 설정

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -3,14 +3,26 @@ import {MdAdd} from 'react-icons/md';
 //https://react-icons.netlify.com/#/icons/md
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
     const [value, setValue] = useState('');
 
     const onChange = useCallback(e => {
         setValue(e.target.value);
     }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue(''); // value값 초기화
+
+            //submit 이벤트는 브라우저에서 새로고침을 발생시킴
+            //이를 방지하기 위해 디폴트 기능을 막는 함수를 호출
+            e.preventDefault();
+        }, [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
+        <form className="TodoInsert" onSubmit={onSubmit}>
             <input placeholder="할 일을 입력하세요" 
             value={value} 
             onChange={onChange}/>
